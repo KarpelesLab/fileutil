66cfaa20299925f5ba0c5c436df615ad466dc03e
Mark Karpeles <magicaltux@gmail.com>
2024-06-16 01:23:29 +0900

    make writefile atomic by suffixing filename with ~ during writing

--- writefile.go
+++ writefile.go
@@ -7,13 +7,18 @@ import (
 
 // WriteFileReader is similar to os.WriteFile but using a Reader instead of a []byte
 func WriteFileReader(name string, data io.Reader, perm os.FileMode) error {
-	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	f, err := os.OpenFile(name+"~", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
 	if err != nil {
 		return err
 	}
+	defer os.Remove(name + "~")
+
 	_, err = io.Copy(f, data)
 	if err1 := f.Close(); err1 != nil && err == nil {
 		err = err1
+	}
+	if err == nil {
+		err = os.Rename(name+"~", name)
 	}
 	return err
 }
